46f0b415ba4426913f68ce8179b0ef01203810c4
Catherine Bronzo <cbronzo@mac.com>
2018-06-28 03:34:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,31 @@
 # code here!
 class School
+  
+attr_accessor :name, :roster
+
   def initialize(name)
     @name = name
-    roster = {}
+    @roster = {}
   end
   
+  def add_student(student, grade)
+    if 
+      @roster[grade]
+      @roster[grade] << student
+    else 
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.collect do |grade, student|
+    @roster[grade].sort!
+    end
+  @roster
+  end
 end
